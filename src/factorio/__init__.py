107bd8184ae46afe603b96d3cffc281af0411495
miragecentury <victorien.vanroye@gmail.com>
2023-08-30 21:12:47 +0200

    feat: add basic structure

---
+++ src/factorio/__init__.py
@@ -0,0 +1,7 @@
+"""
+Main module for the Factorio CLI.
+"""
+
+import typer
+
+typer_application = typer.Typer()
---
+++ src/factorio/__main__.py
@@ -0,0 +1,8 @@
+"""
+Entry point for the Factorio CLI application.
+"""
+
+from . import typer_application
+
+if __name__ == "__main__":
+    typer_application()
