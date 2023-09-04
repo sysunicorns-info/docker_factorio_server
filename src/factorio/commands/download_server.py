fbe54896b31adfdc6f304aee970b77f8b12755e8
miragecentury <victorien.vanroye@gmail.com>
2023-09-04 10:08:47 +0200

    feat(command): add install dir

--- src/factorio/commands/download_server.py
+++ src/factorio/commands/download_server.py
@@ -3,6 +3,8 @@ Provide Command to download Factorio Server
 """
 
 from pathlib import Path
+from tarfile import TarFile
+from typing import Optional
 
 import rich
 import typer
@@ -58,6 +60,8 @@ class FactorioServerDownloaderCommand:
     def download_factorio_server(
         self,
         version: str = typer.Argument(default="latest"),
+        tmp: Optional[Path] = typer.Option(default=None),
+        install_dir: Path = typer.Option(default=Path("/opt/app/factorio")),
     ):
         """
         Download Factorio Server
@@ -72,9 +76,19 @@ class FactorioServerDownloaderCommand:
         except typer.BadParameter as error:
             raise typer.BadParameter(error)
 
+        if tmp is not None:
+            self._factorio_server_downloader_service.set_path(path=tmp)
+
         # Setup the download directory and download the server
-        if not self._factorio_server_downloader_service.download(version=version):
+        _is_success, _path_or_error = self._factorio_server_downloader_service.download(
+            version=version
+        )
+        if not _is_success:
             rich.print(
                 "[red]Error:[/] Could not download Factorio Server",
             )
             raise typer.Exit(code=1)
+
+        # Install the server
+        with TarFile.open(_path_or_error) as _tar_file:
+            _tar_file.extractall(path=install_dir)
